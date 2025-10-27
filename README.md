dd37ad520dfc466fc07d207292e8515ff97b9234
aribrew <arithesage@protonmail.com>
2025-10-27 20:38:44 +0100

    Starting.

---
+++ README.md
@@ -0,0 +1,7 @@
+# [WIP] Base Godot Application
+A starting point for Godot applications.
+
+A 'start' scene and a 'start.gd' script are available by default.
+The project is intended for working without needing the UI.
+
+Edit the "config/name" field in the project.godot file to rename the project.
---
+++ scenes/start.gd
@@ -0,0 +1,39 @@
+extends Node2D
+
+
+
+
+func _ready () -> void:
+	print ("Hello world!")
+
+	
+func _draw () -> void:
+	pass # Used to draw things programatically
+
+
+# The following three functions are used to handle input.
+#
+# There are three because they have different filtering levels:
+# _unhandled_input receives all events not handled by the UI.
+# _unhandled_key_input filters all events except keyboard ones.
+# 
+# Use _input by default, unless you think you need any of the others.
+	
+func _input(event: InputEvent) -> void:
+	pass
+
+
+func _unhandled_input (event: InputEvent) -> void:
+	pass
+
+	
+func _unhandled_key_input (event: InputEvent) -> void:
+	pass
+
+	 
+func _process (delta: float) -> void:
+	pass # App updates here
+
+	
+func _physics_process (delta: float) -> void:
+	pass # Update physics here
