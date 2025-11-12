46c88b0d2e6a7648522b5ac20041e7832ba167b1
aribrew <arithesage@protonmail.com>
2025-11-12 12:32:23 +0100

    Trying to do my own tilemap solution. I find Godot's one too complicated.

--- code/examples/tilemap_example.gd
+++ code/examples/tilemap_example.gd
@@ -8,6 +8,11 @@ class_name TilemapExample
 var tile_w: int = 32
 var tile_h: int = 32
 
+var tilemap_w: int = 64
+var tilemap_h: int = 32
+
+var tilemap: Array
+
 
 func _ready () -> void:
 	var img = Image.new().create_empty (tile_w,
@@ -15,25 +20,24 @@ func _ready () -> void:
 										false,
 										Image.FORMAT_RGB8)
 	img.fill (Color.RED)
+
+
+	var grid = Image.new().create_empty ((tile_w * tilemap_w), 
+										 (tile_h * tilemap_h),
+										 false,
+										 Image.FORMAT_RGB8)
+
+	grid.blend_rect (img,
+					 Rect2i (Vector2i (0,0), img.get_size()),
+					 Vector2i (0,0))
+
+	var grid_tex = ImageTexture.create_from_image (grid)
+
+	var sprite = Sprite2D.new ()
+	sprite.centered = false
+	sprite.texture = grid_tex
+
+	add_child (sprite)
 	
-	var tex = ImageTexture.create_from_image (img)
-
-	var tile_set = TileSet.new ()
-	tile_set.tile_size = Vector2i (tile_w, tile_h)
-	
-	var atlas_source = TileSetAtlasSource.new ()
-	atlas_source.texture = tex
-	atlas_source.texture_region_size = Vector2i (tile_w, tile_h)
-	atlas_source.create_tile (Vector2i (0,0))
-
-	tile_set.add_source (atlas_source)
-
-	var tile_map = TileMap.new ()
-	tile_map.tile_set = tile_set
-	
-	add_child (tile_map)
-
-	tile_map.set_cell (0, Vector2i(0,0), 0)
-
 	print ("Tile map example.")
 
