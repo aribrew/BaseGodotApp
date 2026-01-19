cd8bf117ff0f0daa8b8e04dac65875c15dcef45e
aribrew <arithesage@protonmail.com>
2026-01-19 17:26:29 +0100

    Trying to clean trash. Also, the globals Application and SceneManager weren't defined in this project, and the start scene had the old start.gd script.

--- scenes/start/start.gd
+++ scenes/start/start.gd
@@ -1,13 +1,12 @@
 extends Node2D
 
 # Loads an external script is needed before instancing it
-#const HelloWorld = preload ("res://code/examples/helloworld.gd")
+const HelloWorld = preload ("res://code/examples/helloworld.gd")
 
 
 func _ready () -> void:	
-	# Instance a loaded object
-	#self.add_child (HelloWorld.new())
-
+	self.add_child (HelloWorld.new())
+	
  
 func _process (_delta: float) -> void:
 	pass
