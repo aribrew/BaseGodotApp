07505e1088479f5981de225268ad994cccf4f8f9
aribrew <arithesage@protonmail.com>
2026-02-02 12:00:49 +0100

    Added a scene template with some useful nodes: UI, World (for spawning entities) and SubScenes, to have all things more organized.

--- prefabs/base_scene/base_scene.gd
+++ prefabs/base_scene/base_scene.gd
@@ -1,25 +1,21 @@
-extends Node2D
+extends Node
 
 var UI: CanvasLayer = null
 var World: Node2D = null
 var SubScenes: Node2D = null
 
 
-# Loads an external script is needed before instancing it
-const HelloWorld = preload ("res://code/examples/helloworld.gd")
-
-
-func _ready () -> void:
+func _ready () -> void:	
 	UI = $UI
 	World = $World
 	SubScenes = $SubScenes
-	
-	SubScenes.add_child (HelloWorld.new())
-	
  
+
 func _process (_delta: float) -> void:
 	pass
-	
-	
+
+
 func __spawn (entity: Node2D) -> void:
 	World.add_child (entity)
+	
+	
